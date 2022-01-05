8cc6dd6cb1f84af3ad5ab685036125d1c3917ad5
tennekey <collin.smith283@gmail.com>
2022-01-05 15:42:21 -0500

    Common stock functions

--- src/stockMarketer4S.js
+++ src/stockMarketer4S.js
@@ -1,52 +1,21 @@
-import { localeHHMMSS } from 'common.js'
+import { localeHHMMSS, sellShorts, sellLongs, getStockInfo } from 'common.js'
 
 let stockSymbols
 let corpus
-const commission = 100000
 
 function getMoney(ns) {
-  return ns.getServerMoneyAvailable('home') - 5 * commission
-}
-
-function sellShorts(ns, stockSymbol) {
-  const stockInfo = getStockInfo(ns, stockSymbol)
-  const shortSellValue = ns.stock.sellShort(stockSymbol, stockInfo.sharesShort)
-
-  if (shortSellValue) {
-    corpus += stockInfo.sharesShort * (stockInfo.avgPriceShort - shortSellValue) - 2 * commission
-    ns.print(
-      `[${localeHHMMSS()}][${stockSymbol}] Sold ${stockInfo.sharesShort} shorts for ${ns.nFormat(shortSellValue, '$0.000a')}. Profit: ${ns.nFormat(
-        stockInfo.sharesLong * (stockInfo.avgPriceShort - shortSellValue) - 2 * commission,
-        '$0.000a'
-      )}`
-    )
-  }
-}
-
-function sellLongs(ns, stockSymbol) {
-  const stockInfo = getStockInfo(ns, stockSymbol)
-  const longSellValue = ns.stock.sell(stockSymbol, stockInfo.sharesLong)
-
-  if (longSellValue) {
-    corpus += stockInfo.sharesLong * (longSellValue - stockInfo.avgPriceLong) - 2 * commission
-    ns.print(
-      `[${localeHHMMSS()}][${stockSymbol}] Sold ${stockInfo.sharesLong} longs for ${ns.nFormat(longSellValue, '$0.000a')}. Profit: ${ns.nFormat(
-        stockInfo.sharesLong * (longSellValue - stockInfo.avgPriceLong) - 2 * commission,
-        '$0.000a'
-      )}`
-    )
-  }
+  return ns.getServerMoneyAvailable('home') - 5 * settings().stockCommission
 }
 
 // Only if not going to lose money
 function sellUnderperforming(ns, stockSymbol) {
   const stockInfo = getStockInfo(ns, stockSymbol)
 
-  if (stockInfo.sharesShort && stockInfo.sharesShort * (stockInfo.avgPriceShort - stockInfo.stockAskPrice) > 2 * commission) {
+  if (stockInfo.sharesShort && stockInfo.sharesShort * (stockInfo.avgPriceShort - stockInfo.stockAskPrice) > 2 * settings().stockCommission) {
     sellShorts(ns, stockSymbol)
   }
 
-  if (stockInfo.sharesLong && stockInfo.sharesLong * (stockInfo.stockBidPrice - stockInfo.avgPriceLong) > 2 * commission) {
+  if (stockInfo.sharesLong && stockInfo.sharesLong * (stockInfo.stockBidPrice - stockInfo.avgPriceLong) > 2 * settings().stockCommission) {
     sellLongs(ns, stockSymbol)
   }
 }
@@ -67,7 +36,7 @@ function sellWrongPosition(ns, stockSymbol) {
 
 function buyNewShares(ns, stockSymbol) {
   const stockInfo = getStockInfo(ns, stockSymbol)
-  const minimumMoneyToInvest = 10 * commission
+  const minimumMoneyToInvest = 10 * settings().stockCommission
 
   if (!stockInfo.haveMaxShares && getMoney(ns) > minimumMoneyToInvest) {
     let maxSharesToBuy
@@ -95,39 +64,6 @@ function buyNewShares(ns, stockSymbol) {
       const invested = ns.nFormat(buyValue * sharesToBuy, '$0.000a')
       ns.print(`[${localeHHMMSS()}][${stockSymbol}] Bought ${sharesToBuy} ${shareType} for ${ns.nFormat(buyValue, '$0.000a')} each. Invested: ${invested}`)
     }
-  }
-}
-
-function getStockInfo(ns, stockSymbol) {
-  const [sharesLong, avgPriceLong, sharesShort, avgPriceShort] = ns.stock.getPosition(stockSymbol)
-  const volatility = ns.stock.getVolatility(stockSymbol)
-  const probability = ns.stock.getForecast(stockSymbol) - 0.5
-  const expectedReturn = Math.abs(volatility * probability)
-  const maxShares = ns.stock.getMaxShares(stockSymbol)
-
-  const haveAnyShares = sharesLong + sharesShort > 0
-  const haveMaxShares = sharesLong + sharesShort === maxShares
-
-  const stockAskPrice = ns.stock.getAskPrice(stockSymbol)
-  const stockBidPrice = ns.stock.getBidPrice(stockSymbol)
-
-  const position = probability >= 0 ? 'Long' : 'Short'
-
-  return {
-    stockSymbol,
-    maxShares,
-    haveAnyShares,
-    haveMaxShares,
-    sharesLong,
-    avgPriceLong,
-    stockAskPrice,
-    sharesShort,
-    avgPriceShort,
-    stockBidPrice,
-    volatility,
-    probability,
-    expectedReturn,
-    position,
   }
 }
 
